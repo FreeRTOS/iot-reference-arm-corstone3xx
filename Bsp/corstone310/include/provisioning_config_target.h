5c08d112fce24d68fe2a2bd1e28a8e22774c8160
Devaraj Ranganna <devaraj.ranganna@arm.com>
2023-09-20 14:59:55 +0000

    blinky: Add support for Corstone-310
    Signed-off-by: Devaraj Ranganna <devaraj.ranganna@arm.com>

---
+++ Bsp/corstone310/include/provisioning_config_target.h
@@ -0,0 +1,11 @@
+/* Copyright 2023 Arm Limited and/or its affiliates
+ * <open-source-office@arm.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef _PROVISIONING_CONFIG_TARGET_H_
+#define _PROVISIONING_CONFIG_TARGET_H_
+
+#define PROVISIONING_DATA_START    ( 0x213FF000 )
+
+#endif /* _PROVISIONING_CONFIG_TARGET_H_ */
