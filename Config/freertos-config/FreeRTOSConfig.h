4c6d16bef8e8f3a086114923788af51026f15bca
Devaraj Ranganna <devaraj.ranganna@arm.com>
2023-09-28 12:51:35 +0000

    bsp: Folder restructuring
    The `bsp` folder is restructured to based on target insteading keeping
    the files in `bsp` root folder. In future, this target specific files
    will be moved to `arm-corstone-platform-bsp` repository.
    
    Signed-off-by: Devaraj Ranganna <devaraj.ranganna@arm.com>

--- Config/freertos-config/FreeRTOSConfig.h
+++ Config/freertos-config/FreeRTOSConfig.h
@@ -57,28 +57,9 @@ extern uint32_t SystemCoreClock;
 #define configENABLE_TRUSTZONE              0
 #define configRUN_FREERTOS_SECURE_ONLY      0
 
-/* From the "Fast Models Reference Guide" (https://developer.arm.com/documentation/100964/1123/About-the-models),
- * "Programmer's View (PV) models of processors and devices work at a level
- * where functional behavior is equivalent to what a programmer would see using
- * the hardware.
- *
- * They sacrifice timing accuracy to achieve fast simulation execution speeds:
- * you can use the PV models for confirming software functionality, but you
- * must not rely on the accuracy of cycle counts, low-level component
- * interactions, or other hardware-specific behavior."
- *
- * As described above, FVPs sacrifice timing accuracy to achieve fast
- * simulation execution speeds. Therefore, we need this work around of setting
- * `configTICK_RATE_HZ` to `100` to simulate scheduler polling rate of
- * `1000 Hz` or 1 tick per second.
- *
- * In addition, the macro `pdMS_TO_TICKS` is defined here to match the 1 tick
- * per second instead of using the macro defined in
- * `FreeRTOS-kernel/include/projdefs.h`
- */
-#define configTICK_RATE_HZ                  ( ( uint32_t ) 100 )
-#define pdMS_TO_TICKS( xTimeInMs )    ( ( TickType_t ) xTimeInMs )
-#define TICKS_TO_pdMS( xTicks )       ( ( uint32_t ) xTicks )
+/* The target specific macros `configTICK_RATE_HZ`, `pdMS_TO_TICKS` and
+ * `TICKS_TO_pdMS` are defined in `FreeRTOSConfig_target.h`. */
+#include "FreeRTOSConfig_target.h"
 
 #define configMINIMAL_STACK_SIZE                   4096
 #define configUSE_16_BIT_TICKS                     0
--- Projects/aws-iot-example/provisioning/provisioning_config.h
+++ Projects/aws-iot-example/provisioning/provisioning_config.h
@@ -3,13 +3,18 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include "provisioning_config_target.h"
+
+/* *INDENT-OFF* */
+
 #ifndef _PROVISIONING_CONFIG_H_
 #define _PROVISIONING_CONFIG_H_
 
 #define PROVISIONING_MAGIC               0xC0DEFEED
-#define PROVISIONING_DATA_START          ( 0x210FF000 )
 #define PROVISIONING_DATA_LEN            ( 0x1000 )
 #define PROVISIONING_DATA_HEADER_SIZE    ( 0x4 )
 #define PROVISIONING_PARAM_START         ( PROVISIONING_DATA_START + PROVISIONING_DATA_HEADER_SIZE )
 
-#endif
+#endif /* _PROVISIONING_CONFIG_H_ */
+
+/* *INDENT-ON* */
