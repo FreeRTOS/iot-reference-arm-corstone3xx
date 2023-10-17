e6ec373fdebf250c7d74182d46653b4cc42c7fa2
Ahmed Ismail <Ahmed.Ismail@arm.com>
2023-10-17 13:56:27 +0100

    license: Add missing copyright headers
    Missing copyright headers are added to files.
    
    Signed-off-by: Ahmed Ismail <Ahmed.Ismail@arm.com>

--- Projects/aws-iot-example/freertos-integration-tests/include/unistd.h
+++ Projects/aws-iot-example/freertos-integration-tests/include/unistd.h
@@ -1,1 +1,6 @@
+/* Copyright 2023 Arm Limited and/or its affiliates
+ * <open-source-office@arm.com>
+ * SPDX-License-Identifier: MIT
+ */
+
 /* dummy file for workaround missing ifdefs in AWS qual tests */
