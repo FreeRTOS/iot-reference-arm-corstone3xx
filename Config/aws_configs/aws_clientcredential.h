aa1fb286b2dbfe78f7085dff7a58da8f74f2f6c9
Ahmed Ismail <Ahmed.Ismail@arm.com>
2023-10-26 14:38:32 +0100

    credentials-check: Change to run-time check
    Modify the AWS client credentials check to be part of the
    main application (run-time check) rather than being part of
    the compile-time checks.
    
    This change ease the development process because
    it is no longer needed to modify the aws_clientcredential.h
    file to build the application and then revert it back when
    pushing the changes remotely.
    
    Signed-off-by: Ahmed Ismail <Ahmed.Ismail@arm.com>

--- Config/aws_configs/aws_clientcredential.h
+++ Config/aws_configs/aws_clientcredential.h
@@ -14,31 +14,24 @@
  *
  * For AWS IoT MQTT broker, this is the Thing's REST API Endpoint.
  *
- * @note Your AWS IoT Core endpoint can be found in the AWS IoT console under
+ * @note Replace the used dummy value "dummy.endpointid.amazonaws.com" by your
+ * AWS IoT Core endpoint which can be found in the AWS IoT console under
  * Settings/Custom Endpoint, or using the describe-endpoint REST API (with
  * AWS CLI command line tool).
  *
  */
 
-/* #define clientcredentialMQTT_BROKER_ENDPOINT         "" */
-
-#ifndef clientcredentialMQTT_BROKER_ENDPOINT
-    #error "Uncomment the clientcredentialMQTT_BROKER_ENDPOINT macro above and insert AWS IoT Core endpoint"
-#endif /* clientcredentialMQTT_BROKER_ENDPOINT */
+#define clientcredentialMQTT_BROKER_ENDPOINT    "dummy.endpointid.amazonaws.com"
 
 /**
  * @brief The MQTT client identifier used in this example.  Each client identifier
- * must be unique; so edit as required to ensure that no two clients connecting to
- * the same broker use the same client identifier.
+ * must be unique; so replace the used dummy value "dummy_thingname" as required to
+ * ensure that no two clients connecting to the same broker use the same client identifier.
  *
  * Value is defined in "aws_clientcredential.h".
  */
 
-/* #define clientcredentialIOT_THING_NAME               "" */
-
-#ifndef clientcredentialIOT_THING_NAME
-    #error "Uncomment the clientcredentialIOT_THING_NAME macro above and insert MQTT client identifier"
-#endif /* clientcredentialIOT_THING_NAME */
+#define clientcredentialIOT_THING_NAME               "dummy_thingname"
 
 /**
  * @brief The port to use for the demo.
