4aa70b5211030076cefa1f7ebac6394b7893de71
Hugues Kamba-Mpiana <hugues.kambampiana@arm.com>
2023-12-13 13:21:32 +0000

    coremqtt-agent-mock: Add subset of functionalities
    The added functionalities are what is strictly needed from the
    coreMQTT and coreMQTT Agent modules to test the implementation of the
    FreeRTOS Agent Message module.
    
    Signed-off-by: Hugues Kamba-Mpiana <hugues.kambampiana@arm.com>

---
+++ components/aws_iot/coremqtt_agent/mocks/inc/core_mqtt_agent.h
@@ -0,0 +1,22 @@
+/* Copyright 2023-2024 Arm Limited and/or its affiliates
+ * <open-source-office@arm.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef CORE_MQTT_AGENT_H
+    #define CORE_MQTT_AGENT_H
+
+    #ifdef __cplusplus
+    extern "C" {
+    #endif
+
+    struct MQTTAgentCommand
+    {
+        void * pArgs;
+    };
+
+    #ifdef __cplusplus
+    }
+    #endif
+
+#endif // CORE_MQTT_AGENT_H
