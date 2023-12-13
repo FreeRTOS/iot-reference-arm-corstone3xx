811dfd31368115d66ed88f2f8005571b290b4591
Hugues Kamba-Mpiana <hugues.kambampiana@arm.com>
2023-12-13 12:35:46 +0000

    unit-test-framework: Add tools required to add unit tests
    * GoogleTest is added as the unit test runner
    * Fake Function Framework is added to mock functions
    
    Signed-off-by: Hugues Kamba-Mpiana <hugues.kambampiana@arm.com>

---
+++ tools/cmake/AddUnitTest.cmake
@@ -0,0 +1,16 @@
+# Copyright 2023-2024 Arm Limited and/or its affiliates
+# <open-source-office@arm.com>
+# SPDX-License-Identifier: MIT
+
+if(BUILD_TESTING AND NOT CMAKE_CROSSCOMPILING)
+    include(GoogleTest)
+
+    function(iot_reference_arm_corstone3xx_add_test target)
+        target_link_libraries(${target}
+            PRIVATE
+                GTest::gtest_main
+        )
+
+        gtest_discover_tests(${target})
+    endfunction()
+endif()
