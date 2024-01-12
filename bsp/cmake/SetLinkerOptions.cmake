550a88259307f911c22339b383f218e6f11facc2
Ahmed Ismail <64546783+AhmedIsmail02@users.noreply.github.com>
2024-01-12 18:28:15 +0000

    build: Generate map file for Arm GNU Toolchain (#33)
    Just as is already the case for the Arm Compiler
    for Embedded (ARMClang), generate a map file
    when building applications with the Arm GNU Toolchain.
    
    Signed-off-by: Ahmed Ismail <Ahmed.Ismail@arm.com>

--- bsp/cmake/SetLinkerOptions.cmake
+++ bsp/cmake/SetLinkerOptions.cmake
@@ -9,7 +9,7 @@ macro(set_linker_script executable_target)
             PRIVATE
                 $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone300>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone300/an552_ns.ld>
                 $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone310>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone310/an555_ns.ld>
-                -Wl,--gc-sections
+                -Wl,--gc-sections,-Map=${executable_target}.map
                 --specs=nosys.specs
         )
     else()
