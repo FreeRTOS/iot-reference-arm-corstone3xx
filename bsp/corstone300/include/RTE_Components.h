20f900f47c8df6d494e4e8334b8c08a48f84b800
Dávid Házi <david.hazi@arm.com>
2024-02-23 11:08:41 +0100

    bsp: Update BSP to CMSIS6
    * As CMSIS6 provides VSI IRQ handlers and IRQ definitions,
      the BSP patches related to VSI IRQs are removed.
    
    * BSP patch files that reference the `CMSIS_Driver/Include`
      path have been amended, as the path no longer exists and
      the files patched have moved.
    
    * Changed VSI IRQ handlername to the offical
      ARM_VSI0_Handler
    
    * Added RTE_Components.h which enables USART and MPS3 IO
      CMSIS components.
    
    Signed-off-by: Dávid Házi <david.hazi@arm.com>

---
+++ bsp/corstone300/include/RTE_Components.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019-2024 Arm Limited. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*-------- <<< Use Configuration Wizard in Context Menu >>> -------------------- */
+
+#ifndef __RTE_COMPONENTS_H
+#define __RTE_COMPONENTS_H
+
+/* <q> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART0] */
+/* <i> Configuration settings for Driver_USART0 in component ::Drivers:USART */
+#define   RTE_USART0    1
+
+/* <q> IO (Input- Output) [arm_mps3_io_drv] */
+/* <i> Configuration settings for ARM MPS3 IO SCC in component ::Native Driver:arm_mps3_io_drv */
+#define   RTE_MPS3_IO    1
+
+#endif /* __RTE_COMPONENTS_H */
