03090706bbf78b48619141d4d5e61f0074bc8ecc
Filip Jagodzinski <filip.jagodzinski@arm.com>
2024-06-27 08:18:01 +0000

    cs-300: Extract the ML model to a separate binary
    Eventually, we want the MCUBoot (and the rest of TF-M) to handle the ML
    model in the same way as other components. To achieve that, the ML model
    component will be kept in flash during boot, for the MCUBoot to validate
    the image, and perform the swap operation if needed.
    
    Since the Ethos NPU doesn't have access to flash, the model will be
    copied back to DDR at runtime. This is why the model is still kept in
    the DDR memory region in the linker script.
    
    Patches for the trusted_firmware-m component:
    - Add support for the third image for cs-300 platform.
    - Configure the signing layout for the ML model, and reuse the NS key.
    - Add a modified flash map for the cs-300, for the MCUBoot to handle the
    third image. This is based on the default flash layout used before.
    
    Signed-off-by: Filip Jagodzinski <filip.jagodzinski@arm.com>
    Signed-off-by: Ahmed Ismail <Ahmed.Ismail@arm.com>

--- bsp/cmake/SetLinkerOptions.cmake
+++ bsp/cmake/SetLinkerOptions.cmake
@@ -1,18 +1,29 @@
-# Copyright 2023-2024, Arm Limited and/or its affiliates
+# Copyright 2023-2025, Arm Limited and/or its affiliates
 # <open-source-office@arm.com>
 # SPDX-License-Identifier: Apache-2.0
 
 # Set the linker script for the target specified
 macro(set_linker_script executable_target)
     if(${CMAKE_C_COMPILER_ID} STREQUAL "GNU")
-        target_link_options(${executable_target}
-            PRIVATE
-                $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone300>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone300/an552_ns.ld>
-                $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone310>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone310/an555_ns.ld>
-                $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone315>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone315/corstone_315_ns.ld>
-                $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone320>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone320/corstone_320_ns.ld>
-                -Wl,--gc-sections,-Map=${executable_target}.map
-        )
+        if((${APPLICATION_PATH} MATCHES ".*keyword_detection") AND (${CMAKE_C_COMPILER_ID} STREQUAL "GNU"))
+            target_link_options(${executable_target}
+                PRIVATE
+                    $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone300>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone300/an552_ns_keyword_ml_model_update.ld>
+                    $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone310>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone310/an555_ns_keyword_ml_model_update.ld>
+                    $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone315>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone315/corstone_315_ns_keyword_ml_model_update.ld>
+                    $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone320>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone320/corstone_320_ns_keyword_ml_model_update.ld>
+                    -Wl,--gc-sections,-Map=${executable_target}.map
+            )
+        else()
+            target_link_options(${executable_target}
+                PRIVATE
+                    $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone300>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone300/an552_ns.ld>
+                    $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone310>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone310/an555_ns.ld>
+                    $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone315>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone315/corstone_315_ns.ld>
+                    $<$<STREQUAL:${ARM_CORSTONE_BSP_TARGET_PLATFORM},corstone320>:-T ${IOT_REFERENCE_ARM_CORSTONE3XX_SOURCE_DIR}/bsp/corstone320/corstone_320_ns.ld>
+                    -Wl,--gc-sections,-Map=${executable_target}.map
+            )
+        endif()
     else()
         target_link_options(${executable_target}
             PRIVATE
