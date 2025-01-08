70fdcf3eedad3e6a3958c88c69bef77c588936aa
Gergely Kovacs <Gergely.Kovacs2@arm.com>
2025-01-08 08:39:58 +0000

    build: Blinky application with split-build
    Blinky application with the seperately build TF-M and the new toolchain.
    The cmake files exported by TF-M set up the NS interface. The default
    TF-M signing is used and the resulting images are merged by FRI.
    
    Signed-off-by: Gergely Kovacs <Gergely.Kovacs2@arm.com>

--- components/security/trusted_firmware-m/integration/cmake/MergeTfmImages.cmake
+++ components/security/trusted_firmware-m/integration/cmake/MergeTfmImages.cmake
@@ -2,10 +2,6 @@
 # <open-source-office@arm.com>
 # SPDX-License-Identifier: MIT
 
-include(ExternalProject)
-
-ExternalProject_Get_Property(trusted_firmware-m-build BINARY_DIR)
-
 # To merge the bootloader image, TF-M secure image, non-secure user application image,
 # secure and non-secure provsioning bundle images into one image, their addresses are
 # needed. As the addresses are defined in their respective linker scripts, there is no
@@ -37,20 +33,17 @@ function(iot_reference_arm_corstone3xx_tf_m_merge_images target)
     find_program(srec_cat NAMES srec_cat REQUIRED)
     find_program(objcopy NAMES arm-none-eabi-objcopy objcopy REQUIRED)
     if(ARM_CORSTONE_BSP_TARGET_PLATFORM STREQUAL "corstone300" OR ARM_CORSTONE_BSP_TARGET_PLATFORM STREQUAL "corstone310")
-        add_custom_command(
-            TARGET
-                ${target}
-            POST_BUILD
+        add_custom_target(${target}_merged
             DEPENDS
-                $<TARGET_FILE_DIR:${target}>/${target}_signed.bin
+                ${target}_signed_bin
             COMMAND
-                ${srec_cat} ${BINARY_DIR}/api_ns/bin/bl2.bin -Binary -offset ${BL2_IMAGE_LOAD_ADDRESS}
-                    ${BINARY_DIR}/api_ns/bin/tfm_s_signed.bin -Binary -offset ${S_IMAGE_LOAD_ADDRESS}
-                    $<TARGET_FILE_DIR:${target}>/${target}_signed.bin -Binary -offset ${NS_IMAGE_LOAD_ADDRESS}
+                ${srec_cat} ${CONFIG_SPE_PATH}/bin/bl2.bin -Binary -offset ${BL2_IMAGE_LOAD_ADDRESS}
+                    ${CONFIG_SPE_PATH}/bin/tfm_s_signed.bin -Binary -offset ${S_IMAGE_LOAD_ADDRESS}
+                    ${CMAKE_BINARY_DIR}/bin/${target}_signed.bin -Binary -offset ${NS_IMAGE_LOAD_ADDRESS}
                     ${ddr_binary_param}
                     ${ns_provisioning_data_param}
                     ${model_binary_param}
-                    ${BINARY_DIR}/api_ns/bin/provisioning_bundle.bin -Binary -offset ${S_PROVISIONING_BUNDLE_LOAD_ADDRESS}
+                    ${CONFIG_SPE_PATH}/bin/provisioning_bundle.bin -Binary -offset ${S_PROVISIONING_BUNDLE_LOAD_ADDRESS}
                     -o $<TARGET_FILE_DIR:${target}>/${target}_merged.hex
             COMMAND
                 ${objcopy} -I ihex -O elf32-little
@@ -61,19 +54,16 @@ function(iot_reference_arm_corstone3xx_tf_m_merge_images target)
             VERBATIM
         )
     else()
-        add_custom_command(
-            TARGET
-                ${target}
-            POST_BUILD
+        add_custom_target(${target}_merged
             DEPENDS
-                $<TARGET_FILE_DIR:${target}>/${target}_signed.bin
+                ${target}_signed_bin
             COMMAND
-                ${srec_cat} ${BINARY_DIR}/api_ns/bin/bl1_1.bin -Binary -offset ${BL1_IMAGE_LOAD_ADDRESS}
-                    ${BINARY_DIR}/api_ns/bin/cm_provisioning_bundle.bin -Binary -offset ${S_CM_PROVISIONING_BUNDLE_LOAD_ADDRESS}
-                    ${BINARY_DIR}/api_ns/bin/dm_provisioning_bundle.bin -Binary -offset ${S_DM_PROVISIONING_BUNDLE_LOAD_ADDRESS}
-                    ${BINARY_DIR}/api_ns/bin/bl2_signed.bin -Binary -offset ${BL2_IMAGE_LOAD_ADDRESS}
-                    ${BINARY_DIR}/api_ns/bin/tfm_s_signed.bin -Binary -offset ${S_IMAGE_LOAD_ADDRESS}
-                    $<TARGET_FILE_DIR:${target}>/${target}_signed.bin -Binary -offset ${NS_IMAGE_LOAD_ADDRESS}
+                ${srec_cat} ${CONFIG_SPE_PATH}/bin/bl1_1.bin -Binary -offset ${BL1_IMAGE_LOAD_ADDRESS}
+                    ${CONFIG_SPE_PATH}/bin/cm_provisioning_bundle.bin -Binary -offset ${S_CM_PROVISIONING_BUNDLE_LOAD_ADDRESS}
+                    ${CONFIG_SPE_PATH}/bin/dm_provisioning_bundle.bin -Binary -offset ${S_DM_PROVISIONING_BUNDLE_LOAD_ADDRESS}
+                    ${CONFIG_SPE_PATH}/bin/bl2_signed.bin -Binary -offset ${BL2_IMAGE_LOAD_ADDRESS}
+                    ${CONFIG_SPE_PATH}/bin/tfm_s_signed.bin -Binary -offset ${S_IMAGE_LOAD_ADDRESS}
+                    ${CMAKE_BINARY_DIR}/bin/${target}_signed.bin -Binary -offset ${NS_IMAGE_LOAD_ADDRESS}
                     ${model_binary_param}
                     ${ddr_binary_param}
                     ${ns_provisioning_data_param}
