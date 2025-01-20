2439dc22a76712b32968b90394684b29e0638a56
Ahmed Ismail <Ahmed.Ismail@arm.com>
2025-01-20 17:20:54 +0000

    fri: Address the issues introduced by Ubuntu update
    As a result of updating from Ubuntu v20.04 to v22.04,
    the following changes had to be made:
    
    * The default `setuptools_scm` v8.x requires that
    `setuptools` package has a version higher than 61.
    Hence, updating the minimum required `setuptools`
    version.
    
    * The `license_file` is now deprecated and `license_files`
    option should be used instead.
    
    * The way CI-env-private installs `uncrustify` has been changed
    where it introduced a known issue in the `uncrustify` tool so a
    workaround has been applied to overcome the issue.
    
    Signed-off-by: Ahmed Ismail <Ahmed.Ismail@arm.com>

--- tools/scripts/run_uncrustify.sh
+++ tools/scripts/run_uncrustify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2023-2024 Arm Limited and/or its affiliates
+# Copyright 2023-2025 Arm Limited and/or its affiliates
 # <open-source-office@arm.com>
 # SPDX-License-Identifier: MIT
 
@@ -22,3 +22,8 @@ done
 exclude_pattern+="./build"
 
 fdfind -E $exclude_pattern -e c -e h -e cc -e cpp --exec uncrustify --no-backup --replace --if-changed -c tools/uncrustify.cfg
+# Uncrustify has a known problem where it does not keep track of braces that span multiple preprocessor blocks and therefore will
+# abort and complain about missing braces in code leading to returning an error which is not correct. We use git diff to check if
+# any files are changed after uncrustify runs, where this will return `1` (error) if any files were altered by uncrustify or `0`
+# otherwise.
+git diff --exit-code
