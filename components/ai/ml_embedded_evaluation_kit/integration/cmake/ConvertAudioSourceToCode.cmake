ba9432b42c2c21477ca9fe8d32ede24a30aacbbd
Daniel Velez <daniel.velez@arm.com>
2025-06-17 18:04:55 +0200

    components: Bump MLEK version to 25.05
    The goal of this commit is to update
    ML-Embedded-Evaluation-Kit library to
    version 25.05 which includes switching
    to CMSIS6 from CMSIS5.
    
    Signed-off-by: Daniel Velez <daniel.velez@arm.com>
    Signed-off-by: Ahmed Ismail <Ahmed.Ismail@arm.com>

--- components/ai/ml_embedded_evaluation_kit/integration/cmake/ConvertAudioSourceToCode.cmake
+++ components/ai/ml_embedded_evaluation_kit/integration/cmake/ConvertAudioSourceToCode.cmake
@@ -1,4 +1,4 @@
-# Copyright 2023-2024 Arm Limited and/or its affiliates
+# Copyright 2023-2025 Arm Limited and/or its affiliates
 # <open-source-office@arm.com>
 # SPDX-License-Identifier: MIT
 
@@ -6,16 +6,16 @@
 function(iot_reference_arm_corstone3xx_convert_audio_source_to_code audio_path generated_path)
     add_custom_target(convert-audio
         BYPRODUCTS
-            ${generated_path}/InputFiles.cc
-            ${generated_path}/test.cc
+            ${generated_path}/sample_files.c
+            ${generated_path}/test.c
         # use ml-embedded-evaluation-kit's Python Virtual Environment which
         # contains dependencies for gen_audio_cpp.py
         COMMAND bash -c " \
             source ${CMAKE_CURRENT_BINARY_DIR}/mlek_resources_downloaded/env/bin/activate && \
+            pip install --upgrade soundfile && \
             python3 ${ml_embedded_evaluation_kit_SOURCE_DIR}/scripts/py/gen_audio_cpp.py \
             --audio_path ${audio_path} \
-            --source_folder_path ${generated_path} \
-            --header_folder_path ${generated_path} "
+            --package_gen_dir ${generated_path} "
         VERBATIM
         USES_TERMINAL
     )
